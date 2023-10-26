e45bddeab202807c3a78cf85aea2ed5c7679bc9f
Ricardo Alvarez <ricardo.alvarez@normaltandil.edu.ar>
2023-10-26 09:34:28 -0300

    Modificado menuListados funciones reflexivas

--- Submenues/submenuListados.php
+++ Submenues/submenuListados.php
@@ -2,10 +2,16 @@
 // Función para el submenú de listados
 function menuListados()
 {
+    $opcionesDisponibles = [
+        '1' => 'listarClientes',
+        '2' => 'listarCabanas',
+        '3' => 'listarReservas',
+        '0' => 'menuPrincipal',
+    ];
+
     echo "==================================================\n";
     echo "Menú de Listados";
     echo "\n==================================================\n";
-
     echo "1. Listar Clientes\n";
     echo "2. Listar Cabañas\n";
     echo "3. Listar Reservas\n";
@@ -13,25 +19,11 @@ function menuListados()
 
     $opcion = leerOpcion("Seleccione una opción: ");
 
-    switch ($opcion) {
-        case 1:
-            listarClientes();
-            menuListados();
-            break;
-        case 2:
-            listarCabanas();
-            menuListados();
-            break;
-        case 3:
-            listarReservas();
-            menuListados();
-            break;
-        case 0:
-            menuPrincipal();
-            break;
-        default:
-            echo "Opción inválida. Intente nuevamente.\n";
-            menuListados();
-            break;
+    if (isset($opcionesDisponibles[$opcion])) {
+        $funcion = $opcionesDisponibles[$opcion];
+        call_user_func($funcion);
+    } else {
+        echo "Opción inválida. Intente nuevamente.\n";
+        menuListados();
     }
 }
