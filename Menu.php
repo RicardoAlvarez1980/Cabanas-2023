d20009477e42f7efb6c27dd27a52f440497e47ec
Ricardo Alvarez <ricardo.alvarez@normaltandil.edu.ar>
2023-11-22 10:09:50 -0300

    Agregue las instrucciones para mejorar el sistema!

--- Menu.php
+++ Menu.php
@@ -5,6 +5,12 @@ require_once './Submenues/submenuReserva.php';
 require_once './Submenues/submenuListados.php';
 require_once './Conexion.php';
 
+//1 - Sería Proteger usando integridad referencial.
+//    Al eliminar reservas se elimina cliente y cabaña asociados a la misma
+//2 - Validar fechas. Mostrarlas e ingresarlas en el formato nuestro. 
+//3 - Dadas las fechas de reserva, muestre las cabañas disponibles. 
+//    Buscar cabañas libres dada una fecha en particular
+
 // Arreglos para almacenar cabañas, clientes y reservas
 $cabanas = [];
 $clientes = [];
